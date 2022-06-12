e97f4a7a72f5753817948cf7f96b01513c8d1c53
alphacpc <alphacpc@gmail.com>
2022-06-12 01:06:36 +0000

    Premier commit

---
+++ script-node/index.js
@@ -0,0 +1,59 @@
+const puppeteer = require("puppeteer")
+const csv = require("csv-writer")
+
+
+// FONCTION POUR LA CREATION DU FICHIER CSV
+function create_csv(data){
+    let createCsvWriter = csv.createObjectCsvWriter
+    let csvWriter = createCsvWriter({
+        path : "../data/data-script-js.csv",
+        header : [
+            {id: 'nom', title: 'nom'},
+            {id: 'prix', title: 'prix'},
+            {id: 'poids', title: 'poids'},
+            {id: 'image', title: 'image'},
+            {id: 'site', title: 'site'},
+        ]
+    })
+
+    csvWriter.writeRecords(data)
+    .then( () => console.log("Fichier créé avec succès ! "))
+}
+
+
+// LA FONCTION POUR RECUPER LES DONNEES
+async function getData(){
+    
+    const browser = await puppeteer.launch({headless:true})
+    const page    = await browser.newPage()
+    await page.goto("https://guinarshop.com/shop/")
+    
+    let data =  await page.evaluate( () => {  
+        let results = []
+        let ulItems =  document.querySelector('ul.products.columns-4')
+        let items =  ulItems.querySelectorAll('li')
+
+        items.forEach((item) => {
+
+            let tabword = item.querySelector('h2').innerText.split(' ')
+            
+            results.push({
+                'nom' : item.querySelector('h2').innerText,
+                'prix' : item.querySelector('.woocommerce-Price-amount.amount bdi').innerText,
+                'poids' : tabword[tabword.length -1 ].includes('kg') ? tabword[tabword.length -1 ] : null,
+                'image': item.querySelector('img').src,
+                'site' : window.location.hostname
+            })
+
+        })
+
+        return results
+    })
+
+    console.log(data)
+    create_csv(data)
+    
+    browser.close()
+}
+
+getData()
---
+++ script-python/script.py
@@ -0,0 +1,50 @@
+from bs4 import BeautifulSoup
+from pprint import pprint
+
+import requests
+import csv
+
+url = 'https://baayguins.com/product-category/poulets-frais/'
+response = requests.get(url)
+print(response.status_code)
+
+if response.status_code == 200 :
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+    products_list = soup.find('ul', class_="products columns-4")
+    products = products_list.find_all('li')
+
+
+
+    data = []
+
+    for product in products:
+        dict_data = {}
+
+        title = product.find('h2').getText()
+        prix = product.find('span', class_='price').find('bdi').getText()
+
+        print(prix)
+        print(prix.split('x'))
+
+        dict_data['nom'] = title
+        dict_data['prix'] = prix.replace(u'\xa0', u' ')
+        dict_data['poids'] = title.split('–')[-1]
+        dict_data['image'] = product.find('img', class_='attachment-woocommerce_thumbnail').get('src')
+        dict_data['site'] =  'baayguins.com'
+
+        pprint(dict_data)
+
+        data.append(dict_data)
+
+else:
+
+    print("Adresse Url incorrect !")
+
+
+
+with open('../data/data-script-py.csv', "w") as file:
+
+    donne = csv.DictWriter(file, list(data[0].keys()))
+    donne.writeheader()
+    donne.writerows(data)
